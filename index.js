96ce06ae85ab976183dd895795eee9b1bd2204a3
Heron Ramos <heron@ALFARODOBUS.local>
2024-10-15 11:38:37 -0300

    Adicionando script para movimentação da classe indicator

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const allli = document.querySelectorAll('li')
 allli.forEach( (li, index) => {
     li.addEventListener("click", e => {
         navBar.querySelector(".active-list").classList.remove("active-list")
+        li.classList.add("active-list")
+
+        const indicator = document.querySelector(".indicator")
+        indicator.style.transform = `translateX(calc(${index * 90}px))`
 
     })
 
